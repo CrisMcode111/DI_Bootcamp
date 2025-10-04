e35754cd5dbfc386c4ead0151b42e5830ef7fcb5
Cristina <cristina.moussoungedi@gmail.com>
2025-10-04 14:35:24 +0200

    week1/day1/exercisesxp.py

--- week1/day1/exercisesxp.py
+++ week1/day1/exercisesxp.py
@@ -1,54 +1,54 @@
-# for i in range(4):
-#     print("Hello world")
+for i in range(4):
+    print("Hello world")
 
-# result = (99 ** 3) * 8
-# print(result)
+result = (99 ** 3) * 8
+print(result)
 
-# my_name = "Cristina"
-# name = input ("What's your name,")
-# if name == my_name:
-#    print("You are a princess")
-# else:
-#    print("I don't like you")
+my_name = "Cristina"
+name = input ("What's your name,")
+if name == my_name:
+   print("You are a princess")
+else:
+   print("I don't like you")
 
-# height = 145
-# if height >= 145:
-#     print("You are tall enough to ride!")
-# else:
-#     print("You need to grow some more to ride!")
+height = 145
+if height >= 145:
+    print("You are tall enough to ride!")
+else:
+    print("You need to grow some more to ride!")
 
-# my_fav_numbers = {2,4,6}
-# my_fav_numbers.add(8)
-# my_fav_numbers.remove(8)
-# friend_fav_numbers = {1,3,5}
-# our_fav_numbers = my_fav_numbers and friend_fav_numbers
-
-
-# my_tuple = (1,2,3)
-# print(type(my_tuple))
-
-# my_tuple = (*my_tuple,4,5,6)
-# print(my_tuple)
+my_fav_numbers = {2,4,6}
+my_fav_numbers.add(8)
+my_fav_numbers.remove(8)
+friend_fav_numbers = {1,3,5}
+our_fav_numbers = my_fav_numbers and friend_fav_numbers
 
 
-# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
-# basket.remove("Banana")
-# print(basket)
+my_tuple = (1,2,3)
+print(type(my_tuple))
 
-# basket.remove("Blueberries")
-# print(basket)
+my_tuple = (*my_tuple,4,5,6)
+print(my_tuple)
 
-# basket.append("Kiwi")
-# print(basket)
 
-# basket.insert(0, "Apples")
-# print(basket)
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+print(basket)
 
-# basket.count("Apples")
-# print(basket.count("Apples"))
+basket.remove("Blueberries")
+print(basket)
 
-# basket.clear()
-# print(basket.clear())
+basket.append("Kiwi")
+print(basket)
+
+basket.insert(0, "Apples")
+print(basket)
+
+basket.count("Apples")
+print(basket.count("Apples"))
+
+basket.clear()
+print(basket.clear())
 
 
 sandwich_orders = [
