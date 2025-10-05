811cf5f4b638813ff47aa5a1ca25894fe142e95e
Cristina <cristina.moussoungedi@gmail.com>
2025-10-05 11:30:31 +0200

    week1/day1/dailychallenge.py

--- week1/day1/dailychallenge.py
+++ week1/day1/dailychallenge.py
@@ -1,3 +1,6 @@
+# Ask the user for a number and a length.
+# Create a program that prints a list of multiples of the number until the list length reaches length.
+
 number = input("Tell me a number, please: ")
 length = input("How long should the sequence be? ")
 
@@ -10,7 +13,7 @@ length = 8
 multiples = [number * i for i in range(1, length+1)]
 print(multiples)
 
-
+# Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
 
 word = input("Please enter a word: ")
 
